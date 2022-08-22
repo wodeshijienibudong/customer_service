330f1c9001474c60c001094273020cc0b5ab6ee7
wodeshijienibudong <18772957307@163.com>
2022-08-22 08:56:43 +0800

    客服系统

---
+++ Application/Client/Conf/config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'OFFICIAL_WEB' => 'http://www.duiler.com',
+	'ClIENT_HOST' => 'http://www.duiler.com',
+    'CLIENT_WS' => 'ws://www.duiler.com:8585'
+);
---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	'MODULE_ALLOW_LIST'    =>    array('Manage','Client','Chat','Home'),
+	'DEFAULT_MODULE'       =>    'Home',
+	'URL_MODEL'          => '2',
+
+	'OFFICIAL_WEBSITE' => 'localhost',
+	//安全过滤
+    'DEFAULT_FILTER' => 'strip_tags,htmlspecialchars,magicAddslashes,trim',
+		//数据库连接
+    
+
+);
